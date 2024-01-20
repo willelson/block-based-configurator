4cf10582b4a8a3142ed895f26dde74097d75d6f8
Will Elson <elson594@gmail.com>
2024-01-20 06:25:23 +0000

    Vue app init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="area-container">
+    <ConfigArea />
+    <OperatorsPanel />
+  </div>
+</template>
+
+<script>
+import ConfigArea from '@/components/configurator/ConfigArea.vue'
+import OperatorsPanel from '@/components/configurator/OperatorsPanel.vue'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    ConfigArea,
+    OperatorsPanel
+  },
+  methods: {}
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/configurator/OperatorsPanel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="area-container">operators panel</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  components: {},
+  methods: {}
+}
+</script>
+
+<style scoped></style>
