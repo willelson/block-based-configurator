97ffd5ba257720308197328526829888281dc35d
Will Elson <elson594@gmail.com>
2024-01-23 06:17:30 +0100

    Droppable area wrapper component

---
+++ src/components/configurator/DroppableArea.vue
@@ -0,0 +1,16 @@
+<template>
+  <div @drop="onDrop($event)" @dragenter.prevent @dragover.prevent>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['drop'],
+  methods: {
+    onDrop(event) {
+      this.$emit('drop', event)
+    }
+  }
+}
+</script>
